512b5f426af56467ba523d6b129af4e3bfba3132
markwst <wongsathorn_w@cmu.ac.th>
2022-08-03 23:58:03 +0700

    5.5 & 5.6

--- lab04_5/src/router/index.js
+++ lab04_5/src/router/index.js
@@ -3,65 +3,63 @@ import HomeView from '../views/HomeView.vue'
 import EventDetails from '../views/event/EventDetail.vue'
 import Airlines from '../views/event/EventAirline.vue'
 import EventLayout from '../views/event/EventLayout.vue'
-const routes = [{
-        path: '/',
-        name: 'home',
-        component: HomeView,
-        props: (route) => ({
-            page: parseInt(route.query.page) || 1,
-            perPage: parseInt(route.query.perPage) || 3
-        })
-    },
-    {
-        path: '/about',
-        name: 'about',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import ( /* webpackChunkName: "about" */ '../views/AboutView.vue')
-    },
+import NotFoundView from '../views/NotFoundView.vue'
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+    props: (route) => ({
+      page: parseInt(route.query.page) || 1,
+      perPage: parseInt(route.query.perPage) || 3
+    })
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
 
-    {
-        path: '/passenger/:id',
-        name: 'EventLayout',
-        props: true,
-        component: EventLayout,
-        children: [{
-                path: '',
-                name: 'EventDetails',
-                component: EventDetails,
-                props: true
-            },
-            {
-                path: 'airline',
-                name: 'airlines',
-                component: Airlines,
-                prop: true
-            }
-        ]
-    },
-    {
-        path: '/404/:resource',
-        name: '404Resource',
-        component: NotFoundView,
+  {
+    path: '/passenger/:id',
+    name: 'EventLayout',
+    props: true,
+    component: EventLayout,
+    children: [
+      {
+        path: '',
+        name: 'EventDetails',
+        component: EventDetails,
         props: true
-    },
-    {
-        path: '/network-error',
-        name: 'NetworkError',
-        component: NetWorkErrorView
-    },
-    {
-        path: '/:catchAll(.*)',
-        name: 'NotFound',
-        component: NotFoundView
-    }
+      },
+      {
+        path: 'airline',
+        name: 'airlines',
+        component: Airlines,
+        prop: true
+      }
+    ]
+  },
+  {
+    path: '/404/:resource',
+    name: '404Resource',
+    component: NotFoundView,
+    props: true
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFoundView
+  }
 ]
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes
+  history: createWebHistory(process.env.BASE_URL),
+  routes
 })
 
 export default router
